824c26cd8e63fd7960b1e09ab3dcdaaf92b2d540
Xu Sun <sunxuhit@gmail.com>
2014-08-29 15:42:05 -0700

    Modified centrality macro to have Default and String Melting Mode. Modified submit script to have Default and String Melting Mode.

--- submit/pdsf/Default/Resolution.sh
+++ submit/pdsf/Default/Resolution.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 date
 
-#. ./Flow.sh
+#. ./Resolution.sh
 
 if [ $# -eq 0 ]
   then
 #    counter=0
-    Name="_Flow_"
+    Name="_Resolution_Default_"
     suffix=".root"
-    for((counter=0;counter<=9;counter=counter+1))
+    for((counter=0;counter<=0;counter=counter+1))
     do
       cp ./run.csh ./run$Name$counter.csh
 
-      echo -n "root4star -b -q -x 'Flow.C(" >> run$Name$counter.csh
+      echo -n "root4star -b -q -x 'Resolution.C(" >> run$Name$counter.csh
 ###############################energy###################################
 #      echo -n 0',' >> run$Name$counter.csh  # 7GeV
 #      echo -n 1',' >> run$Name$counter.csh  # 11GeV
 #      echo -n 2',' >> run$Name$counter.csh  # 19GeV
 #      echo -n 3',' >> run$Name$counter.csh  # 27GeV
-      echo -n 4',' >> run$Name$counter.csh  # 39GeV
-#      echo -n 5',' >> run$Name$counter.csh  # 62GeV
+#      echo -n 4',' >> run$Name$counter.csh  # 39GeV
+      echo -n 5',' >> run$Name$counter.csh  # 62GeV
 #      echo -n 6',' >> run$Name$counter.csh  # 200GeV
 ###############################energy###################################
+
+
+#############################AMPT_Mode##################################
+      echo -n 0',' >> run$Name$counter.csh  # Default
+#      echo -n 1',' >> run$Name$counter.csh  # String Melting
+#############################AMPT_Mode##################################
 
 ################################List####################################
       echo -n $counter',' >> run$Name$counter.csh # List
@@ -33,8 +39,8 @@ if [ $# -eq 0 ]
 ############################start_event#################################
 
 #############################stop_event#################################
-      echo  1000000000')'"'" >> run$Name$counter.csh  # stop_event
-#      echo  100024')'"'" >> run$Name$counter.csh  # stop_event: test mode
+#      echo  1000000000')'"'" >> run$Name$counter.csh  # stop_event
+      echo  100024')'"'" >> run$Name$counter.csh  # stop_event: test mode
 #############################stop_event#################################
 
       qsub -hard -l scratchfree=500,h_cpu=24:00:00,h_vmem=1.8G,projectio=1 -o /project/projectdirs/star/xusun/OutPut/AMPT_Default/Log/run$Name$counter.log -e /project/projectdirs/star/xusun/OutPut/AMPT_Default/Log/run$Name$counter.err ./run$Name$counter.csh
