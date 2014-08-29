d566381de4c2eac919dec19122138eda0ba6a1a4
Xu Sun <sunxuhit@gmail.com>
2014-08-29 15:50:58 -0700

    Add comments in Flow.C and Resolution.C

--- Resolution.C
+++ Resolution.C
@@ -1,5 +1,6 @@
 #include <TSystem>
 
+// root4star -b -q Resolution.C\(3,1,0,1,1024\)
 void Resolution(const Int_t Energy = 3, const Int_t Mode = 1, const Int_t List = 0, const Long64_t StartEvent = 1000, const Long64_t StopEvent = 10024) // Energy = 0: 7GeV, 1: 11GeV, 2: 19GeV, 3: 27GeV, 4: 39GeV, 5: 62GeV, 6: 200 GeV | Mode = 0: Default, 1: String Melting
 {
   gSystem->Load("AMPT_resolution");
