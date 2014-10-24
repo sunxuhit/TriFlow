412ab30375569b40a0e44bd9bbf5a3c1b946adc0
Xu Sun <sunxuhit@gmail.com>
2014-10-24 19:05:58 +0800

    DiffFlow v1.0: Calculate pT differential ratio between v2 and v3. Decrease the bin number and range for flow and spectra to decrease error bar.

--- DiffFlow.C
+++ DiffFlow.C
@@ -25,15 +25,15 @@ static TString Centrality[4] = {"0080","0010","1040","4080"};
 static TString ParType[10] = {"pi_plus","pi_minus","K_plus","K_minus","p","pbar","Lambda","Lambdabar","K0s","phi"};
 
 // Calculate integrated v2 and v3
-void InteFlow(Int_t mEnergy = 4, Int_t mMode = 0) // 0: 7.7 GeV, 1: 11.5 GeV, 2: 19.6 GeV, 3: 27 GeV, 4: 39 GeV, 5: 62.4 GeV, 6: 200 GeV | 0: Default, 1: String Melting
+void DiffFlow(Int_t mEnergy = 4, Int_t mMode = 0) // 0: 7.7 GeV, 1: 11.5 GeV, 2: 19.6 GeV, 3: 27 GeV, 4: 39 GeV, 5: 62.4 GeV, 6: 200 GeV | 0: Default, 1: String Melting
 {
   //---------------------Constant------------------------
   const Float_t pt_start = 0.2; // start point for pt integration
   const Float_t pt_stop  = 3.0; // stop point for pt integration
   //---------------------Constant------------------------
 
-  TString inputfile = Form("/home/xusun/Data/AMPT_%s/Flow/%s_%s/Flow_%s.root",Mode[mMode].Data(),Energy[mEnergy].Data(),Mode[mMode].Data(),Energy[mEnergy].Data());
-//  TString inputfile = Form("/home/xusun/Data/AMPT_%s/Flow/%s_%s/Flow_%s_1_100.root",Mode[mMode].Data(),Energy[mEnergy].Data(),Mode[mMode].Data(),Energy[mEnergy].Data()); // temperory file
+//  TString inputfile = Form("/home/xusun/Data/AMPT_%s/Flow/%s_%s/Flow_%s.root",Mode[mMode].Data(),Energy[mEnergy].Data(),Mode[mMode].Data(),Energy[mEnergy].Data());
+  TString inputfile = Form("/home/xusun/Data/AMPT_%s/Flow/%s_%s/Flow_%s.root",Mode[mMode].Data(),Energy[mEnergy].Data(),Mode[mMode].Data(),Energy[mEnergy].Data()); // temperory file
   cout << "Input File: " << inputfile.Data() << endl;
   TFile *File_input = TFile::Open(inputfile.Data());
 
@@ -71,48 +71,32 @@ void InteFlow(Int_t mEnergy = 4, Int_t mMode = 0) // 0: 7.7 GeV, 1: 11.5 GeV, 2:
 	  h_mFlow[i_par][i_order][i_cent]->SetBinContent(i_bin,p_mFlow[i_par][i_order][i_cent]->GetBinContent(i_bin));
 	  h_mFlow[i_par][i_order][i_cent]->SetBinError(i_bin,p_mFlow[i_par][i_order][i_cent]->GetBinError(i_bin));
 	}
-	HistName = Form("Pt_%s_%s_%s",ParType[i_par].Data(),Order[i_order].Data(),Centrality[i_cent].Data());
-	h_mPt[i_par][i_order][i_cent] = (TH1F*)File_input->Get(HistName.Data());
-	h_mFlow[i_par][i_order][i_cent]->Multiply(h_mPt[i_par][i_order][i_cent]);
 	//--------------------------------------------------------------------------------------------------
       }
     }
   }
 
-  // Calculate Integrated v2 and v3 => could be merged into loop above => using individual loop to be more clear
-  for(Int_t i_order = 0; i_order < 2; i_order++)
+  // Calculate differential ratio v2/v3 => could be merged into loop above => using individual loop to be more clear
+  TH1F *h_ratio[10][4];
+  for(Int_t i_par = 0; i_par < 10; i_par++)
   {
     for(Int_t i_cent = 0; i_cent < 4; i_cent++)
     {
-      TString HistName = Form("h_mInteFlow_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mInteFlow[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),10,-0.5,9.5);
-      for(Int_t i_par = 0; i_par < 10; i_par++)
-      {
-	Double_t Inte_flow = 0.0;
-	Double_t Err_flow  = 0.0;
-	Double_t Inte_spec = 0.0;
-	Double_t Err_spec  = 0.0;
-	Inte_flow = h_mFlow[i_par][i_order][i_cent]->IntegralAndError(h_mFlow[i_par][i_order][i_cent]->FindBin(pt_start),h_mFlow[i_par][i_order][i_cent]->FindBin(pt_stop),Err_flow,"width");
-	Inte_spec = h_mPt[i_par][i_order][i_cent]->IntegralAndError(h_mPt[i_par][i_order][i_cent]->FindBin(pt_start),h_mPt[i_par][i_order][i_cent]->FindBin(pt_stop),Err_spec,"width");
-	if(Inte_spec != 0.0)
-	{
-	  Double_t Err_ratio = ErrDiv(Inte_flow,Inte_spec,Err_flow,Err_spec);
-	  h_mInteFlow[i_order][i_cent]->SetBinContent(i_par+1,Inte_flow/Inte_spec);
-	  h_mInteFlow[i_order][i_cent]->SetBinError(i_par+1,Err_ratio);
-	}
-      }
+      TString HistName = Form("h_ratio_%s_%s",ParType[i_par].Data(),Centrality[i_cent].Data());
+      h_ratio[i_par][i_cent] = (TH1F*)h_mFlow[i_par][0][i_cent]->Clone(HistName.Data());
+      h_ratio[i_par][i_cent]->Divide(h_mFlow[i_par][1][i_cent]);
     }
   }
 
   // write Histogram into Output file
-  TString outputfile = Form("/home/xusun/Data/AMPT_%s/InteFlow/%s_%s/InteFlow.root",Mode[mMode].Data(),Energy[mEnergy].Data(),Mode[mMode].Data(),Energy[mEnergy].Data());
+  TString outputfile = Form("/home/xusun/Data/AMPT_%s/InteFlow/%s_%s/DiffFlow.root",Mode[mMode].Data(),Energy[mEnergy].Data(),Mode[mMode].Data(),Energy[mEnergy].Data());
   TFile *File_output = new TFile(outputfile.Data(),"RECREATE");
   File_output->cd();
-  for(Int_t i_order = 0; i_order < 2; i_order++)
+  for(Int_t i_par = 0; i_par < 10; i_par++)
   {
     for(Int_t i_cent = 0; i_cent < 4; i_cent++)
     {
-      h_mInteFlow[i_order][i_cent]->Write();
+      h_ratio[i_par][i_cent]->Write();
     }
   }
 
