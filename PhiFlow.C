a7e8cb39806091fc3c8a3d5496d75f4e0be08bf4
Xu Sun <sunxuhit@gmail.com>
2014-11-07 11:23:28 +0800

    v0.3: modify macro to run the AMPT_phi

--- PhiFlow.C
+++ PhiFlow.C
@@ -1,7 +1,7 @@
 #include <TSystem>
 
 // root4star -b -q PhiFlow.C\(4,0,0,1,1024\)
-void PhiFlow(const Int_t Energy = 4, const Int_t Mode = 0, const Int_t List = 0, const Long64_t StartEvent = 1000, const Long64_t StopEvent = 10024) // Energy = 0: 7GeV, 1: 11GeV, 2: 19GeV, 3: 27GeV, 4: 39GeV, 5: 62GeV | Mode = 0: Default, 1: String Melting
+void PhiFlow(const Int_t Energy = 4, const Int_t Mode = 0, const Int_t List = 0, const Long64_t StartEvent = 1000, const Long64_t StopEvent = 10024, const Int_t Flag_ME = 0) // Energy = 0: 7GeV, 1: 11GeV, 2: 19GeV, 3: 27GeV, 4: 39GeV, 5: 62GeV | Mode = 0: Default, 1: String Melting | Flag_ME = 0: Same Event, 1: Mixed Event
 {
   gSystem->Load("AMPT_phi");
 
