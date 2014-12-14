5e54fa04edec7e53d983e083b55fd2dbc5e30d65
Xu Sun <sunxuhit@gmail.com>
2014-12-14 13:29:39 +0800

    v5.7: increase the upper limit for the list => 2500

--- StRoot/AMPT_phi/AMPT_phi.h
+++ StRoot/AMPT_phi/AMPT_phi.h
@@ -50,8 +50,8 @@ class AMPT_phi
     static TString mMode_AMPT[2];
     static TString mMode_SM[2];
     static Int_t mRefMult[2][7][10]; // centrality definition
-    static Int_t mList_start[20];
-    static Int_t mList_stop[20];
+    static Int_t mList_start[25];
+    static Int_t mList_stop[25];
     static Int_t cent_low[4];
     static Int_t cent_up[4];
     static Int_t Centrality_start;
--- StRoot/AMPT_v3v2/AMPT_v3v2.h
+++ StRoot/AMPT_v3v2/AMPT_v3v2.h
@@ -41,8 +41,8 @@ class AMPT_v3v2 // TODO: change the Tree structure
     static TString mBeamEnergy[7];
     static TString mMode_AMPT[2];
     static Int_t mRefMult[2][7][10]; // centrality definition
-    static Int_t mList_start[20];
-    static Int_t mList_stop[20];
+    static Int_t mList_start[25];
+    static Int_t mList_stop[25];
     static Int_t cent_low[4];
     static Int_t cent_up[4];
     static Int_t Centrality_start;
