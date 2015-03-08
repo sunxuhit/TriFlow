3394ae36accf00874e8ae2bb7f4575eed56bd0d7
Xu Sun <sunxuhit@gmail.com>
2015-03-08 14:45:07 +0800

    v7.1: switch binning from 0.1-0.3 to 0.2-0.4

--- StRoot/AMPT_v3v2/AMPT_v3v2.cxx
+++ StRoot/AMPT_v3v2/AMPT_v3v2.cxx
@@ -176,85 +176,85 @@ void AMPT_v3v2::Init()
 
       // flow relative to event plane
       ProName = Form("Flow_pi_plus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // pi_plus
-      p_mFlow_pi_plus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_pi_plus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_pi_plus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_pi_plus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_pi_plus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_pi_minus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // pi_minus
-      p_mFlow_pi_minus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_pi_minus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_pi_minus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_pi_minus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_pi_minus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_K_plus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // K_plus
-      p_mFlow_K_plus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_K_plus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_K_plus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_K_plus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_K_plus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_K_minus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // K_minus
-      p_mFlow_K_minus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_K_minus[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_K_minus_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_K_minus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_K_minus[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_p_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // p
-      p_mFlow_p[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_p[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_p_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_p[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_p[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_pbar_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // pbar
-      p_mFlow_pbar[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_pbar[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_pbar_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_pbar[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_pbar[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_Lambda_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // Lambda
-      p_mFlow_Lambda[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_Lambda[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_Lambda_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_Lambda[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_Lambda[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_Lambdabar_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // Lambdabar
-      p_mFlow_Lambdabar[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_Lambdabar[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_Lambdabar_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_Lambdabar[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_Lambdabar[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_K0s_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // K0s
-      p_mFlow_K0s[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_K0s[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_K0s_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_K0s[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_K0s[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_phi_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data()); // phi
-      p_mFlow_phi[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_phi[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
       HistName = Form("Pt_phi_%s_%s",Order[i_order].Data(),Centrality[i_cent].Data());
-      h_mPt_phi[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.1,5.1);
+      h_mPt_phi[i_order][i_cent] = new TH1F(HistName.Data(),HistName.Data(),25,0.0,5.0);
 
       // flow relative to Reaction Plane
       ProName = Form("Flow_pi_plus_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // pi_plus
-      p_mFlow_pi_plus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_pi_plus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_pi_minus_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // pi_minus
-      p_mFlow_pi_minus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_pi_minus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_K_plus_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // K_plus
-      p_mFlow_K_plus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_K_plus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_K_minus_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // K_minus
-      p_mFlow_K_minus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_K_minus_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_p_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // p
-      p_mFlow_p_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_p_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_pbar_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // pbar
-      p_mFlow_pbar_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_pbar_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_Lambda_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // Lambda
-      p_mFlow_Lambda_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_Lambda_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_Lambdabar_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // Lambdabar
-      p_mFlow_Lambdabar_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_Lambdabar_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_K0s_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // K0s
-      p_mFlow_K0s_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_K0s_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
 
       ProName = Form("Flow_phi_%s_%s_RP",Order[i_order].Data(),Centrality[i_cent].Data()); // phi 
-      p_mFlow_phi_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.1,5.1);
+      p_mFlow_phi_RP[i_order][i_cent] = new TProfile(ProName.Data(),ProName.Data(),25,0.0,5.0);
     }
   }
 
