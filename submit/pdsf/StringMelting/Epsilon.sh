443de698b492b75f6debcacbb5c8c1e30dc1506d
Xu Sun <sunxuhit@gmail.com>
2015-03-10 12:44:41 +0800

    v7.4: add submittion scripts for epsilon calculation

---
+++ submit/pdsf/StringMelting/Epsilon.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+date
+
+#. ./Epsilon.sh
+
+if [ $# -eq 0 ]
+  then
+#    counter=0
+    Name="_11GeV_Epsilon_Str1_"
+    for((counter=0;counter<=19;counter=counter+1))
+    do
+      cp ./run.csh ./run$Name$counter.csh
+
+      echo -n "root4star -b -q -x 'Epsilon.C(" >> run$Name$counter.csh
+###############################energy###################################
+#      echo -n 0',' >> run$Name$counter.csh  # 7GeV
+      echo -n 1',' >> run$Name$counter.csh  # 11GeV
+#      echo -n 2',' >> run$Name$counter.csh  # 19GeV
+#      echo -n 3',' >> run$Name$counter.csh  # 27GeV
+#      echo -n 4',' >> run$Name$counter.csh  # 39GeV
+#      echo -n 5',' >> run$Name$counter.csh  # 62GeV
+#      echo -n 6',' >> run$Name$counter.csh  # 200GeV
+###############################energy###################################
+
+
+##############################AMPT_Mode#################################
+#      echo -n 0',' >> run$Name$counter.csh  # Default
+      echo -n 1',' >> run$Name$counter.csh  # String Melting
+##############################AMPT_Mode#################################
+
+##############################Screen_Mode#################################
+      echo -n 0',' >> run$Name$counter.csh  # 1.5mb
+#      echo -n 1',' >> run$Name$counter.csh  # 3mb
+#      echo -n 2',' >> run$Name$counter.csh  # 6mb
+##############################AMPT_Mode#################################
+
+################################List####################################
+      echo -n $counter',' >> run$Name$counter.csh # List
+################################List####################################
+
+
+############################start_event#################################
+      echo -n 0',' >> run$Name$counter.csh  # start_event
+############################start_event#################################
+
+#############################stop_event#################################
+      echo  1000000000')'"'" >> run$Name$counter.csh  # stop_event
+#      echo  100024')'"'" >> run$Name$counter.csh  # stop_event: test mode
+#############################stop_event#################################
+
+      qsub -hard -l scratchfree=500,h_cpu=24:00:00,h_vmem=1.8G,projectio=1 -o /project/projectdirs/star/xusun/OutPut/AMPT_StringMelting/Log/run$Name$counter.log -e /project/projectdirs/star/xusun/OutPut/AMPT_StringMelting/Log/run$Name$counter.err ./run$Name$counter.csh
+
+      mv run$Name$counter.csh /project/projectdirs/star/xusun/OutPut/AMPT_StringMelting/Script/
+    done
+
+  else
+    echo "Wrong number of parameters"
+fi
