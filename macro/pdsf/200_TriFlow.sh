94c7b404c91b9ad63e633e0debcf93fde6f15025
Xu Sun <sunxuhit@gmail.com>
2015-12-02 16:26:05 +0800

    v2.54: change log output for TriFlow

--- macro/pdsf/200_TriFlow.sh
+++ macro/pdsf/200_TriFlow.sh
@@ -36,12 +36,9 @@ if [ $# -eq 0 ]
 ###############################energy###################################
 
 ###############################flag_ME###################################
-      echo -n 0')' >> run$Name$counter.csh  # Same Event 
-#      echo -n 1')' >> run$Name$counter.csh  # Mixed Event
+      echo -n 0')'"'" >> run$Name$counter.csh  # Same Event 
+#      echo -n 1')'"'" >> run$Name$counter.csh  # Mixed Event
 ###############################flag_ME###################################
-      echo -n "' >! /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu200GeV/Log/Lambda/run" >> run$Name$counter.csh
-      echo -n $Name$counter >> run$Name$counter.csh
-      echo ".log" >> run$Name$counter.csh
 
       qsub -hard -l projectio=1,scratchfree=500,h_cpu=24:00:00,h_vmem=1.8G -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu200GeV/Log/Lambda/job$Name$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu200GeV/Log/Lambda/job$Name$counter.err ./run$Name$counter.csh
 
