066ea205c0e0a9e5fd8ad1bb971b1988bb3eed3c
Xu Sun <sunxuhit@gmail.com>
2016-01-12 13:38:38 +0800

    v0.61_SysErrors: modified hadd_all_final.sh

--- macro/pdsf/hadd_all_final.sh
+++ macro/pdsf/hadd_all_final.sh
@@ -10,34 +10,30 @@ if [ $# -eq 0 ]
     List_SM=SE
     SM=_${List_SM}_
     Energy=200GeV
-    OutDir="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/$PID/flow_$PID/merged_file/MERGED_Yields$SM${Energy}_"
+    OutDir="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/$PID/flow_$PID/merged_file/Yields$SM${Energy}"
     suffix=".root"
     InPutList="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/List/${PID}_list/flow_List/flow_${List_SM}_final.list"
-    counter=0
-      cp ./run_hadd.csh ./run_hadd_final_$PID$SM$counter.csh
-      echo "cd ./AuAu$Energy/$PID/flow_$PID/merged_file/" >> run_hadd_final_$PID$SM$counter.csh
-      echo " " >> run_hadd_final_$PID$SM$counter.csh
+      cp ./run_hadd.csh ./run_hadd_final_$PID$SM.csh
+      echo "cd ./AuAu$Energy/$PID/flow_$PID/merged_file/" >> run_hadd_final_$PID$SM.csh
+      echo " " >> run_hadd_final_$PID$SM.csh
 
-      OutName=$OutDir$counter$suffix
+      OutName=$OutDir$suffix
 
-      echo "rm $OutName " >> run_hadd_final_$PID$SM$counter.csh
-      echo " " >> run_hadd_final_$PID$SM$counter.csh
-      echo -n "hadd $OutName " >> run_hadd_final_$PID$SM$counter.csh
+      echo "rm $OutName " >> run_hadd_final_$PID$SM.csh
+      echo " " >> run_hadd_final_$PID$SM.csh
+      echo -n "hadd $OutName " >> run_hadd_final_$PID$SM.csh
 
       for item in `cat $InPutList`
       do
-	echo -n "$item" >> run_hadd_final_$PID$SM$counter.csh
-
-	echo " " >> run_hadd_final_$PID$SM$counter.csh
-	echo " " >> run_hadd_final_$PID$SM$counter.csh
-	echo "echo 'This is the end of hadd\!\!\!'" >> run_hadd_final_$PID$SM$counter.csh
+	echo -n "$item" >> run_hadd_final_$PID$SM.csh
       done
+      echo " " >> run_hadd_final_$PID$SM.csh
+      echo " " >> run_hadd_final_$PID$SM.csh
+      echo "echo 'This is the end of hadd\!\!\!'" >> run_hadd_final_$PID$SM.csh
 
-      qsub -hard -l scratchfree=500,h_cpu=24:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_$PID$SM$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_$PID$SM$counter.err ./run_hadd_final_$PID$SM$counter.csh
+      qsub -hard -l scratchfree=500,h_cpu=24:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_$PID$SM.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_$PID$SM.err ./run_hadd_final_$PID$SM.csh
 
-      mv run_hadd_final_$PID$SM$counter.csh /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Script/hadd/
-      let "counter=counter+1"
-
+      mv run_hadd_final_$PID$SM.csh /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Script/hadd/
   else
     echo "Wrong number of parameters"
 fi
