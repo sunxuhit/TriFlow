b2132c5326fffcd5d2a637542a8e125bb8543a59
Xu Sun <sunxuhit@gmail.com>
2016-04-12 15:24:14 +0800

    v0.95_SysErrors: add hadd macro of flow and yield for 2nd and final round

---
+++ macro/pdsf/hadd_PiKP_Yield_final.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+date
+
+#. ./hadd_PiKP_Yield_final.sh 
+
+
+if [ $# -eq 0 ]
+  then
+    PID=Proton
+    Energy=200GeV
+    OutDir="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Mass2_$PID/Yields/merged_file/Yield_${Energy}_"
+    suffix=".root"
+    InPutList="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/List/${PID}_list/yield_List/yield_${PID}_final.list"
+    counter=0
+    for item in `cat $InPutList`
+    do
+      cp ./run_hadd.csh ./run_hadd_final_${PID}_Yields_$counter.csh
+      echo "cd ./AuAu$Energy/Mass2_$PID/Yields/merged_file/" >> run_hadd_final_${PID}_Yields_$counter.csh
+      echo " " >> run_hadd_final_${PID}_Yields_$counter.csh
+
+      OutName=$OutDir$counter$suffix
+
+      echo "rm $OutName " >> run_hadd_final_${PID}_Yields_$counter.csh
+      echo " " >> run_hadd_final_${PID}_Yields_$counter.csh
+      echo -n "hadd $OutName " >> run_hadd_final_${PID}_Yields_$counter.csh
+
+      for yields in `cat $item`
+      do
+	echo -n "$yields " >> run_hadd_final_${PID}_Yields_$counter.csh
+      done
+
+      echo " " >> run_hadd_final_${PID}_Yields_$counter.csh
+      echo " " >> run_hadd_final_${PID}_Yields_$counter.csh
+      echo "echo 'This is the end of hadd\!\!\!'" >> run_hadd_final_${PID}_Yields_$counter.csh
+
+      qsub -hard -l scratchfree=500,h_cpu=2:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_final_${PID}_Yields_$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_final_${PID}_Yields_$counter.err ./run_hadd_final_${PID}_Yields_$counter.csh
+
+      mv run_hadd_final_${PID}_Yields_$counter.csh /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Script/hadd/
+      let "counter=counter+1"
+    done
+
+  else
+    echo "Wrong number of parameters"
+fi
---
+++ macro/pdsf/hadd_PiKP_final.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+date
+
+#. ./hadd_PiKP_final.sh 
+
+
+if [ $# -eq 0 ]
+  then
+    PID=Proton
+    Energy=200GeV
+    OutDir="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Mass2_$PID/merged_file/Flow_${Energy}_"
+    suffix=".root"
+    InPutList="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/List/${PID}_list/flow_List/flow_${PID}_final.list"
+    counter=0
+    for item in `cat $InPutList`
+    do
+      cp ./run_hadd.csh ./run_hadd_final_${PID}_$counter.csh
+      echo "cd ./AuAu$Energy/Mass2_$PID/merged_file/" >> run_hadd_final_${PID}_$counter.csh
+      echo " " >> run_hadd_final_${PID}_$counter.csh
+
+      OutName=$OutDir$counter$suffix
+
+      echo "rm $OutName " >> run_hadd_final_${PID}_$counter.csh
+      echo " " >> run_hadd_final_${PID}_$counter.csh
+      echo -n "hadd $OutName " >> run_hadd_final_${PID}_$counter.csh
+
+      for yields in `cat $item`
+      do
+	echo -n "$yields " >> run_hadd_final_${PID}_$counter.csh
+      done
+
+      echo " " >> run_hadd_final_${PID}_$counter.csh
+      echo " " >> run_hadd_final_${PID}_$counter.csh
+      echo "echo 'This is the end of hadd\!\!\!'" >> run_hadd_final_${PID}_$counter.csh
+
+      qsub -hard -l scratchfree=500,h_cpu=2:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_final_${PID}_$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_final_${PID}_$counter.err ./run_hadd_final_${PID}_$counter.csh
+
+      mv run_hadd_final_${PID}_$counter.csh /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Script/hadd/
+      let "counter=counter+1"
+    done
+
+  else
+    echo "Wrong number of parameters"
+fi
