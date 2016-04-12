99a06238cccf3d1f8ae260f35ffa2b06efa136ba
Xu Sun <sunxuhit@gmail.com>
2016-04-12 17:29:35 +0800

    v0.96_SysErrors: fix bugs of final round of hadd PiKP

--- macro/pdsf/hadd_PiKP_Yield_final.sh
+++ macro/pdsf/hadd_PiKP_Yield_final.sh
@@ -8,25 +8,22 @@ if [ $# -eq 0 ]
   then
     PID=Proton
     Energy=200GeV
-    OutDir="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Mass2_$PID/Yields/merged_file/Yield_${Energy}_"
+    OutDir="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/Mass2_$PID/Yields/merged_file/Yield_${Energy}"
     suffix=".root"
     InPutList="/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu$Energy/List/${PID}_list/yield_List/yield_${PID}_final.list"
-    counter=0
-    for item in `cat $InPutList`
-    do
       cp ./run_hadd.csh ./run_hadd_final_${PID}_Yields_$counter.csh
       echo "cd ./AuAu$Energy/Mass2_$PID/Yields/merged_file/" >> run_hadd_final_${PID}_Yields_$counter.csh
       echo " " >> run_hadd_final_${PID}_Yields_$counter.csh
 
-      OutName=$OutDir$counter$suffix
+      OutName=$OutDir$suffix
 
       echo "rm $OutName " >> run_hadd_final_${PID}_Yields_$counter.csh
       echo " " >> run_hadd_final_${PID}_Yields_$counter.csh
       echo -n "hadd $OutName " >> run_hadd_final_${PID}_Yields_$counter.csh
 
-      for yields in `cat $item`
+      for item in `cat $InPutList`
       do
-	echo -n "$yields " >> run_hadd_final_${PID}_Yields_$counter.csh
+	echo -n "$item " >> run_hadd_final_${PID}_Yields_$counter.csh
       done
 
       echo " " >> run_hadd_final_${PID}_Yields_$counter.csh
@@ -36,9 +33,6 @@ if [ $# -eq 0 ]
       qsub -hard -l scratchfree=500,h_cpu=2:00:00,h_vmem=1.8G,projectio=1 -o /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_final_${PID}_Yields_$counter.log -e /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Log/hadd/job_final_${PID}_Yields_$counter.err ./run_hadd_final_${PID}_Yields_$counter.csh
 
       mv run_hadd_final_${PID}_Yields_$counter.csh /global/project/projectdirs/starprod/rnc/xusun/OutPut/AuAu${Energy}/Script/hadd/
-      let "counter=counter+1"
-    done
-
-  else
-    echo "Wrong number of parameters"
+    else
+      echo "Wrong number of parameters"
 fi
